f038f5825cc5f7216098bbcdcee19f988927ca74
wacho91 <criskol.71@gmail.com>
2025-01-07 13:36:06 -0500

    Files board

---
+++ src/Store/Board.jsx
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+import { boardData } from "../Data/Index";
+
+const useBoard = create((set) => {
+    board: boardData,
+    setBoard: (board)=> set((state)=> ({board}))
+
+})
+
+export default useBoard
