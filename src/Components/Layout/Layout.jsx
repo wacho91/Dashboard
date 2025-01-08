112a704d4b7d8b3d015bda189da60bb89816e715
wacho91 <criskol.71@gmail.com>
2025-01-08 11:27:43 -0500

    Layout Component

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import moment from "moment/moment"
+import css from "./Layout.module.css"
+import { BiSearch } from "react-icons/bi"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
+
+
+const Layout = () => {
+  return (
+    <div>
+        Layout Component
+    </div>
+  )
+}
+
+export default Layout
