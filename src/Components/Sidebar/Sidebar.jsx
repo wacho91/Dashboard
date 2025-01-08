495d8ac1c89a5af90e2425b39bcca732cd6fe7dc
wacho91 <criskol.71@gmail.com>
2025-01-08 11:47:24 -0500

    Arquitecture component sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,9 +1,31 @@
-
+import { MdSpaceDashboard } from "react-icons/md"
+import { AiFillCalendar, AiOutlineTable } from "react-icons/ai"
+import  { FaTasks } from "react-icons/fa"
+import { NavLink } from "react-router-dom"
 
 const Sidebar = () => {
   return (
     <div>
-        Sidebar Component
+        <img src="../../../public/logo.png" alt="logo" />
+
+        <div>
+
+           <NavLink to="dashboard" title="dashboard">
+                <MdSpaceDashboard size={30}/>
+           </NavLink>
+           
+           <NavLink to="calendar" title="calendar">
+                <AiFillCalendar size={30}/>
+           </NavLink>
+
+           <NavLink to="board" title="board">
+                <FaTasks size={30}/>
+           </NavLink>
+
+           <NavLink to="users" title="users">
+                <AiOutlineTable size={30}/>
+           </NavLink>
+        </div>
     </div>
   )
 }
