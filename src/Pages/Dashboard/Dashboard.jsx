e6d6b6e9f3a16a731e0987fc301b55268cf6d03b
wacho91 <criskol.71@gmail.com>
2025-01-09 11:46:47 -0500

    Dashboard Component

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Orders from "../../Components/Orders/Orders"
+import Statistics from "../../Components/Statistics/Statistics"
+import { cardsData, groupNumber } from "../../Data/Index"
+
+
+const Dashboard = () => {
+  return (
+    <div>
+        Dashboard Component
+    </div>
+  )
+}
+
+export default Dashboard
