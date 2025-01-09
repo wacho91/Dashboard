c0a5fb7b929bf8545396287671d2f8f9758f3e59
wacho91 <criskol.71@gmail.com>
2025-01-09 12:01:29 -0500

    Arquitecture Dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,11 +4,42 @@ import { cardsData, groupNumber } from "../../Data/Index"
 
 
 const Dashboard = () => {
-  return (
+  return <div>
+    {/* left side */}
     <div>
-        Dashboard Component
+        <div>
+            <div>
+                <span>Dashboard</span>
+                <div>
+                    <select>
+                        <option value="">1 week</option>
+                        <option value="">1 month</option>
+                        <option value="">1 year</option>
+                    </select>
+                </div>
+            </div>
+            <div>
+                {
+                    cardsData.map((card, index) => (
+                        <div key={index}>
+                            <div>
+                                <span>{card.title}</span>
+                                <span>+{card.change}</span>
+                            </div>
+
+                            <div>
+                                <span>$</span>
+                                <span>{groupNumber(card.amount)}</span>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+        <Statistics />
     </div>
-  )
+        <Orders />
+  </div>
 }
 
 export default Dashboard
