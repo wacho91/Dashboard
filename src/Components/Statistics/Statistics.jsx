3f4369e55df60dff16418b38dc2865a5075d8f2b
wacho91 <criskol.71@gmail.com>
2025-01-09 10:23:50 -0500

    Statistics Component

---
+++ src/Components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+
+import { BsArrowUpShort } from "react-icons/bs"
+import { groupNumber } from "../../Data/Index"
+
+const Statistics = () => {
+  return (
+    <div>
+      Statistics Compoents
+    </div>
+  )
+}
+
+export default Statistics
