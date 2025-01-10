9158b1adeebde959f8447ee0e79aea1932ffef3a
wacho91 <criskol.71@gmail.com>
2025-01-10 10:52:17 -0500

    AddCardModel Component

---
+++ src/Components/AddCardModal/AddCardModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCardModal = () => {
+  return (
+    <div>
+        AddModalCard Component
+    </div>
+  )
+}
+
+export default AddCardModal
--- src/Pages/Board/Board.jsx
+++ src/Pages/Board/Board.jsx
@@ -1,4 +1,11 @@
 
+import Board, { moveCard, moveColumn, removeCard, addCard } from "@asseinfo/react-kanban"
+import "@asseinfo/react-kaban/dist/style.css"
+import useBoard from "../../Store/Board"
+import { RxCross2 } from "react-icons/rx"
+import { IoMdAdd } from "react-icons/io"
+import { useState } from "react"
+
 
 const Board = () => {
   return (
