029e48bf3813fc80c146878878446be8ad5bcfa9
wacho91 <criskol.71@gmail.com>
2025-01-10 11:15:03 -0500

    AddCardModule component complete

--- src/Components/AddCardModal/AddCardModal.jsx
+++ src/Components/AddCardModal/AddCardModal.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
 
-const AddCardModal = () => {
+import { useState } from "react"
+import Rodal from "rodal"
+import "rodal/lib/rodal.css"
+import css from "./AddCardModal.module.css"
+
+const AddCardModal = ( { visible, onClose, handleCardAdd } ) => {
+
+    const customStyles = {
+        background: "rgb(58 58 58)",
+        padding: "20px",
+        width: "50%",
+        top: "-3rem",
+        height: "fit-content",
+        maxWidth: "40rem"
+
+    }
+
+    const [title, setTitle] = useState('')
+    const [detail, setDetail] = useState('')
+
   return (
-    <div>
-        AddModalCard Component
-    </div>
+    <Rodal customStyles={customStyles} visible={visible} onClose={onClose} >
+        <div className={css.container}>
+            <div>
+                <span className={css.label}>Card Title</span>
+                <input type="text" className={css.input} value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div>
+                <span className={css.label}>Detail</span>
+                <textarea 
+                    rows={10} value={detail}
+                    type="text" onChange={(e) => setDetail(e.target.value)}
+                />
+            </div>
+
+            <button
+                disabled={title=== "" && detail === ""}
+                className={css.saveButton}
+                onClick={()=> {
+                    handleCardAdd(title, detail)
+                    setDetail("")
+                    setTitle("")
+                }}
+            >
+                Add
+            </button>
+        </div>
+    </Rodal>
   )
 }
 
